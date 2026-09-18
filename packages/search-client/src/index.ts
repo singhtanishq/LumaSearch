c6d17c61d060222b3eab315e80efcb8eeff5bf93
Tanishq Singh <tanishqsingh@outlook.in>
2026-09-19 01:49:22 +0530

    Add index.ts

---
+++ packages/search-client/src/index.ts
@@ -0,0 +1,13 @@
+export { createSearchClient, checkSearchHealth } from './client';
+export type { SearchClientConfig } from './client';
+export { IndexManager } from './index-manager';
+export type { IndexManagerOptions } from './index-manager';
+export {
+  mappingFor,
+  webIndexMapping,
+  codeIndexMapping,
+  docsIndexMapping,
+  verticalIndexName,
+} from './mappings';
+export type { MappingOptions } from './mappings';
+export * from './search';
