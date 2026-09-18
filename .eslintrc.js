834b356280b6072f84a67d1db7da9813818308f8
Tanishq Singh <tanishqsingh@outlook.in>
2026-09-19 01:45:53 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,50 +4,44 @@ module.exports = {
     node: true,
     es2022: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended-type-checked',
-    'plugin:@typescript-eslint/stylistic-type-checked',
-    'prettier',
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
   ignorePatterns: [
     'node_modules/',
     'dist/',
     'build/',
     '.turbo/',
     '.next/',
-    '*.config.*',
+    'coverage/',
     '*.d.ts',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: ['./tsconfig.json', './packages/*/tsconfig.json', './apps/*/tsconfig.json'],
-    tsconfigRootDir: __dirname,
   },
   plugins: ['@typescript-eslint', 'import'],
   rules: {
-    '@typescript-eslint/array-type': ['error', { default: 'array-simple' }],
-    '@typescript-eslint/consistent-type-imports': 'error',
     '@typescript-eslint/no-unused-vars': [
       'error',
       { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
     ],
-    '@typescript-eslint/no-floating-promises': 'warn',
-    '@typescript-eslint/no-misused-promises': 'warn',
-    '@typescript-eslint/require-await': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
     'import/order': [
-      'error',
+      'warn',
       {
         groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
-        pathGroups: [
-          { pattern: '@luma-search/**', group: 'internal' },
-        ],
+        pathGroups: [{ pattern: '@luma-search/**', group: 'internal' }],
         pathGroupsExcludedImportTypes: ['builtin', 'external'],
-        alphabetize: { order: 'asc' },
         'newlines-between': 'always',
       },
     ],
   },
+  overrides: [
+    {
+      files: ['**/*.test.ts', '**/*.test.tsx'],
+      rules: {
+        '@typescript-eslint/no-non-null-assertion': 'off',
+      },
+    },
+  ],
 };
