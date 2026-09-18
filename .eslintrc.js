1e2b5c21d635e7d8c37706cd7163baf75140007f
Tanishq Singh <tanishqsingh@outlook.in>
2026-09-18 18:46:29 +0530

    Add .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2022: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'prettier',
+  ],
+  ignorePatterns: [
+    'node_modules/',
+    'dist/',
+    'build/',
+    '.turbo/',
+    '.next/',
+    '*.config.*',
+    '*.d.ts',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './packages/*/tsconfig.json', './apps/*/tsconfig.json'],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint', 'import'],
+  rules: {
+    '@typescript-eslint/array-type': ['error', { default: 'array-simple' }],
+    '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+    ],
+    '@typescript-eslint/no-floating-promises': 'warn',
+    '@typescript-eslint/no-misused-promises': 'warn',
+    '@typescript-eslint/require-await': 'warn',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        pathGroups: [
+          { pattern: '@luma-search/**', group: 'internal' },
+        ],
+        pathGroupsExcludedImportTypes: ['builtin', 'external'],
+        alphabetize: { order: 'asc' },
+        'newlines-between': 'always',
+      },
+    ],
+  },
+};
